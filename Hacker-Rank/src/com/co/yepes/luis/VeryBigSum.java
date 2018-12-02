1c890a81ee36e25fc4a62684d65c471952582705
yepesalvarez <yepesluisfernando@hotmail.com>
2018-12-01 23:06:54 -0500

    Added solved 'Very Big Sum'

---
+++ Hacker-Rank/src/com/co/yepes/luis/VeryBigSum.java
@@ -0,0 +1,51 @@
+package com.co.yepes.luis;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VeryBigSum {
+
+	// Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+    	
+    return Arrays.stream(ar).mapToObj(BigInteger::valueOf)
+    		.reduce(BigInteger.ZERO, (a, b) -> a.add(b))
+    		.longValueExact();
+    	
+    }
+
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("testassets\\veryBigSum.txt"));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
